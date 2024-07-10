72889101b07be4dd078e9cbb3a3b3af4c2e315f0
Harsh Shuddhalwar <168475079+HarshTCP1111@users.noreply.github.com>
2024-07-11 02:56:19 +0530

    Add files via upload

---
+++ Heal-Me-Fit-Front-End/mvp Design/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Slider from './Slider';
+import Login from './Login';
+import '../App.css'; // Adjust the path if necessary
+
+const App = () => {
+  return (
+    <div className="container">
+      <Slider />
+      <Login />
+    </div>
+  );
+};
+
+export default App;
